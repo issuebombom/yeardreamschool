83c77f7d2b7a876bed1b7c8832ae7b61fb3c2517
issuebombom <79882498+issuebombom@users.noreply.github.com>
2022-05-03 18:22:24 +0900

    Update answer.py

--- codingtest/전화번호 목록/answer.py
+++ codingtest/전화번호 목록/answer.py
@@ -1,1 +1,9 @@
+# 성공
+def solution(phone_book):
+    phone_book.sort()
 
+    for i, j in zip(phone_book, phone_book[1:]): 
+        if j.startswith(i):
+            return False
+        
+    return True
